4a68a0ea54bc43c823af35283d82cfe9277c4167
Milos Schlessing <121901670+MilosSchlessing@users.noreply.github.com>
2023-12-05 11:03:36 +0100

    webserver deployment

--- src/main/java/de/htwberlin/webtech/classes/WebtechApplication.java
+++ src/main/java/de/htwberlin/webtech/classes/WebtechApplication.java
@@ -1,4 +1,4 @@
-package de.htwberlin.webtech;
+package de.htwberlin.webtech.classes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
