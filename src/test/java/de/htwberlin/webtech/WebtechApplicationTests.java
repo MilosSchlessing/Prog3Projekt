20dda87a56e67ac01d891b8082946560fadd742d
Milos Schlessing <121901670+MilosSchlessing@users.noreply.github.com>
2023-12-05 13:25:14 +0100

    webserver deployment 2

--- src/test/java/de/htwberlin/webtech/WebtechApplicationTests.java
+++ src/test/java/de/htwberlin/webtech/WebtechApplicationTests.java
@@ -1,6 +1,7 @@
 package de.htwberlin.webtech;
 
 import de.htwberlin.webtech.classes.WebtechApplication;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = WebtechApplication.class)
+@Disabled
 class WebtechApplicationTests {
 	@Test
 	void contextLoads() {
